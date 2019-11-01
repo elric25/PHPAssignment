49306c9d52a3ec8cf54c2535cc751e00c1cf071f
elric25 <boud0137@algonquinlive.com>
2019-11-01 09:50:55 -0400

    Replaced footer
    Replaced in-line footer with using the seperate footer file, we should be using external footer/header on each page.

--- Index.php
+++ Index.php
@@ -105,12 +105,14 @@ and open the template in the editor.
 
     <div class="push"></div>
   </div>
- <footer class="footer">
+<!-- <footer class="footer">
       <div class="row" style="background-color: green; padding-bottom: 0px; padding: 15px;">
       <div class="col-md-5"></div>
       <div class="col-md-6"><label class="footerText">@Algonquin College 2010 - 2019. All Rights Reserved</label></div>
       </div>    
- </footer>
+ </footer>-->
+<?php include('./ProjectCommon/Footer.php'); ?>
+
        
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
