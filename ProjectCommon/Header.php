326b77ab4d97ec830ad9b6c4f4358dbb85c3e4c9
Tetiana Sobchyshyna <43253717+TetianaSob@users.noreply.github.com>
2019-11-21 20:27:52 -0500

    Update Header.php
    Header

--- ProjectCommon/Header.php
+++ ProjectCommon/Header.php
@@ -1,16 +1,5 @@
-<!DOCTYPE html>
-<html lang="en" style="position: relative; min-height: 100%;">
-<html>
-    <head>
-        <title>Project</title>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="main.scss"/>
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-   
-    </head>
-<body>
-        <!-- Nav Bar -->       
+        <!-- Nav Bar --> 
+
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
 <img src="images/AC.png" alt="Italian Trulli">
   
@@ -21,28 +10,33 @@
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
       <li class="nav-item active">
-          <a class="nav-link" href="Index.php" id="homePage" name="homePage">Home</a>
+          <a class="nav-link" href="Index.php" id="homePage" name="homePage"><strong>Home</strong> <span class="sr-only">(current)</span></a>
       </li>
       <!-- nav link 1 -->
+
       <li class="nav-item">
-          <a class="nav-link" href="MyFriends.php" id="myFriends" name="myFrriends">My Friends</a>
+          <a class="nav-link" href="MyFriends.php" id="myFriends" name="myFriends">My Friends</a>
       </li>
       <!-- nav link 2 -->
       <li class="nav-item">
-          <a class="nav-link" href="MyAlbums.php" id="myAlbums" name="myAlbums"><strong>My Albums</strong><span class="sr-only">(current)</span></a>
+          <a class="nav-link" href="MyAlbums.php" id="myAlbums" name="myAlbums">My Albums</a>
       </li>
-            <!-- nav link 3 -->
+       <!-- nav link 3-->
       <li class="nav-item">
           <a class="nav-link" href="MyPictures.php" id="myPictures" name="myPictures">My Pictures</a>
       </li>
-                  <!-- nav link 4 -->
+             <!-- nav link 4-->
       <li class="nav-item">
-          <a class="nav-link" href="Login.php" id="login" name="login">Login</a>
+          <a class="nav-link" href="UploadPictures.php" id="uploadPictures" name="uploadPictures">Upload Pictures</a>
+      </li>
+                  <!-- nav link 5 -->
+                 
+      <li class="nav-item">
+          <a class="nav-link" href="Login.php" id="login" name="login">Log in</a>
       </li>
     </ul>
 
   </div>
 </nav>
 
-</body>
-</html>
+ <!-- end of nav bar -->  
