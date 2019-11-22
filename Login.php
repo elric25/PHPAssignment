60a3bdd1761c81a245a31e4a5684279c33daa700
Tetiana Sobchyshyna <43253717+TetianaSob@users.noreply.github.com>
2019-11-21 20:31:05 -0500

    Login.php
    Login.php

--- Login.php
+++ Login.php
@@ -1,27 +1,25 @@
 <!DOCTYPE html>
+<!--Header -->
+<?php include('./ProjectCommon/Header.php'); ?>  
+<!--Footer -->
 
-            <?php
-          
-            session_start();
-            //$connection = ConnectDb();
-            include('./ProjectCommon/Header.php');
-            include ('./ProjectCommon/Functions.php'); 
+<?php
+/* start of the session */
+session_start();
+include ('./ProjectCommon/Functions.php');
+$connection = ConnectDb();
 
 $userId = $_POST["userId"];
 $password = $_POST["password"];
 $btnSubmit = $_POST["submit"];
 $information = "";
 
-// if($_SESSION["login"]=="yes")
-// {
-//     header("Login: Logout.php");
-// }
 if (isset($_POST['btnSubmit'])) {
     //information
-    $information = ValidateLogin($userId, $password, $information);
+    $information = ValidateLogin($userId, $password);
     if ($information == "") {
         $_SESSION["login"] = $userId;
-        header("Location: Index.php");
+        header("Location: CurrentRegistration.php");
     }
 }
 ?>
@@ -49,7 +47,7 @@ if (isset($_POST['btnSubmit'])) {
                                 <a class="aditionalInformationLink" href="NewUser.php" id="newUser" name="newUser" style="color:blue; weight: bold; font-size: 20px;">sign up</a>
                                 if you are a new user.
                             </p>
-                            <p class='error' style="color:red; weight: bold"><?php echo "$information"; ?>
+                            <p class="text-danger"><?php echo "$information"; ?>
                             </p>
                         </div>
                     </div>
@@ -118,14 +116,14 @@ if (isset($_POST['btnSubmit'])) {
                 </div>
             </form>             
         </div>
+<?php include('./ProjectCommon/Footer.php'); ?>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 
-        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-
-        <!-- front part end -->
+<!-- front part end -->
 
 
-    </body>     
+</body>     
 </html>
 
