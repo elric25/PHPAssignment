6dc309df8c24f71c60b6a043c9ce82b39fc90363
Tetiana Sobchyshyna <43253717+TetianaSob@users.noreply.github.com>
2019-11-24 16:32:56 -0500

    FriendPictures.php
    FriendPictures.php

--- FriendPictures.php
+++ FriendPictures.php
@@ -61,27 +61,80 @@
                 <!-- 2------------------Picture -->   
                 <div class="row">
                     <!-- label --> 
-                    <div class="col-md-2"></div>
+                    <div class="col-md-1"></div>
                     <div class="col-md-6" id="inputText">
-                        <img src="images/Canadian-Parliament-3.jpg" width="900" title="imageFromDatabase" alt="imageFromDatabase">
+                        <img src="images/Canadian-Parliament-3.jpg" width="800" title="imageFromDatabase" alt="imageFromDatabase">
                         <br>
-                        <div class="scrollmenu" width="800">
+                                                <div class="scrollmenu" width="800">
 
-                            <a href="" class="imageScroll"><img src="images/Canadian-Parliament-3.jpg" alt="" id="imgScroll"></a>
-                            <a href="" class="imageScroll"><img src="images/gettyimages-514479606.jpg" alt="" id="imgScroll"></a>
-                            <a href="" class="imageScroll"><img src="images/chateau-frontenac.jpg" alt="" id="imgScroll"></a>
-                            <a href="" class="imageScroll"><img src="images/Chateau-Frontenac-Quebec.jpg" alt="" id="imgScroll"></a>
-                            <a href="" class="imageScroll"><img src="images/altar.jpg" alt="" id="imgScroll"></a>
-                            <a href="" class="imageScroll"><img src="images/Summer-2015-Skyline0_5e954389-5056-a36f-234589b46e9b25ae.jpg" alt="" id="imgScroll"></a>
-                            <a href="" class="imageScroll"><img src="images/torontoskyline_8-250763.jpg" alt="" id="imgScroll"></a>
-                            <a href="" class="imageScroll"><img src="images/wallpaper-tags-toronto-night-city-city-lights-share-this-wallpaper.jpg" alt="" id="imgScroll"></a>
-                            <a href="" class="imageScroll"><img src="images/Untitled-design-40.jpg" alt="" id="imgScroll"></a>
-                            <a href="" class="imageScroll"><img src="images/5b.jpg" alt="" id="imgScroll"></a>
-                            <a href="" class="imageScroll"><img src="images/1200px-AGO_at_dusk.jpg" alt="" id="imgScroll"></a>
-                            <a href="" class="imageScroll"><img src="images/ROM_Exterior_406EC7A3-BE4C-4C08-83BF1C20A4F195D2_c63e8e8b-1e48-457f-bc3fd5ee7f1b9563.jpg" alt="" id="imgScroll"></a>
-                            <a href="" class="imageScroll"><img src="images/shutterstock_673595569.jpg" alt="" id="imgScroll"></a>
-                            <a href="" class="imageScroll"><img src="images/royal-ontario-museum-wedding-1.jpg" alt="" id="imgScroll"></a>
+                            <a href="" class="imageScroll"><img src="images/Canadian-Parliament-3.jpg" alt="" width="200px" height="120px" id="imgScroll"></a>
+                            <a href="" class="imageScroll"><img src="images/gettyimages-514479606.jpg" alt="" width="200px" height="120px" id="imgScroll"></a>
+                            <a href="" class="imageScroll"><img src="images/chateau-frontenac.jpg" alt="" width="200px" height="120px" id="imgScroll"></a>
+                            <a href="" class="imageScroll"><img src="images/Chateau-Frontenac-Quebec.jpg" alt="" width="200px" height="120px" id="imgScroll"></a>
+                            <a href="" class="imageScroll"><img src="images/altar.jpg" alt="" width="200px" height="120px" id="imgScroll"></a>
+                            <a href="" class="imageScroll"><img src="images/Summer-2015-Skyline0_5e954389-5056-a36f-234589b46e9b25ae.jpg" alt="" width="200px" height="120px" id="imgScroll"></a>
+                            <a href="" class="imageScroll"><img src="images/torontoskyline_8-250763.jpg" alt="" width="200px" height="120px" id="imgScroll"></a>
+                            <a href="" class="imageScroll"><img src="images/wallpaper-tags-toronto-night-city-city-lights-share-this-wallpaper.jpg" alt="" width="200px" height="120px" id="imgScroll"></a>
+                            <a href="" class="imageScroll"><img src="images/Untitled-design-40.jpg" alt="" width="200px" height="120px" id="imgScroll"></a>
+                            <a href="" class="imageScroll"><img src="images/5b.jpg" alt="" width="200px" height="120px" id="imgScroll"></a>
+                            <a href="" class="imageScroll"><img src="images/1200px-AGO_at_dusk.jpg" width="200px" height="120px" alt="" id="imgScroll"></a>
+                            <a href="" class="imageScroll"><img src="images/ROM_Exterior_406EC7A3-BE4C-4C08-83BF1C20A4F195D2_c63e8e8b-1e48-457f-bc3fd5ee7f1b9563.jpg" alt="" width="200px" height="120px" id="imgScroll"></a>
+                            <a href="" class="imageScroll"><img src="images/shutterstock_673595569.jpg" alt="" width="200px" height="120px" id="imgScroll"></a>
+                            <a href="" class="imageScroll"><img src="images/royal-ontario-museum-wedding-1.jpg" alt="" width="200px" height="120px" id="imgScroll"></a>
                         </div>
+
+    <style>
+        div.scrollmenu {
+  background-color: #333;
+  overflow: auto;
+  white-space: nowrap;
+  
+  height:140px;
+ width: 900px;
+ margin-bottom: 50px;
+}
+
+div.scrollmenu a {
+  display: inline-block;
+  color: white;
+  text-align: center;
+  padding: 1px;
+  text-decoration: none;
+}
+
+div.scrollmenu a:hover {
+  background-color: #777;
+}
+    </style>
+                        <style>
+
+.photobanner {
+ height: 233px;
+ width: 900px;
+ margin-bottom: 50px;
+}
+ 
+.imageScroll {
+ -webkit-transition: all 0.5s ease;
+ -moz-transition: all 0.5s ease;
+ -o-transition: all 0.5s ease;
+ -ms-transition: all 0.5s ease;
+ transition: all 0.5s ease;
+}
+ 
+.imageScroll:hover {
+ -webkit-transform: scale(1.1);
+ -moz-transform: scale(1.1);
+ -o-transform: scale(1.1);
+ -ms-transform: scale(1.1);
+ transform: scale(1.1);
+ cursor: pointer;
+ 
+ -webkit-box-shadow: 0px 3px 5px rgba(0,0,0,0.2);
+ -moz-box-shadow: 0px 3px 5px rgba(0,0,0,0.2);
+ box-shadow: 0px 3px 5px rgba(0,0,0,0.2);
+}
+                        </style>
                         <!-- Image slides -->
 
                     </div>
