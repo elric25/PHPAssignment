1dea6f7c2ef9d77908cc67b79a13bfb386b21768
Tetiana Sobchyshyna <43253717+TetianaSob@users.noreply.github.com>
2019-11-24 14:26:17 -0500

    Login.php
    Login.php

--- Login.php
+++ Login.php
@@ -26,6 +26,7 @@ if (isset($_POST['btnSubmit'])) {
 
 <!-- front end start-->
 <html>
+
     <body style="background-color: rgba(130, 181, 224, 0.8)">
         <div class="wrapper">
             <br>
@@ -106,16 +107,12 @@ if (isset($_POST['btnSubmit'])) {
                     <div class="col-md-1"><button type="submit" id="btnSubmit" name="btnSubmit" class="btn btn-primary">Submit</button></div>
                     <div class="col-md-1"><button type="submit" id="btnClear" name="btnClear" class="btn btn-primary">Clear</button></div>
                 </div>
-            </form>             
-        </div>
+</form>
+     
+    <div class="push"></div>
+  </div>
+
 <?php include('./ProjectCommon/Footer.php'); ?>
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-
-<!-- front part end -->
-
-
 </body>     
 </html>
 
