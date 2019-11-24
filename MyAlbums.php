16fe4b24f835e611552ce7d4fc83a7de7b2650b7
Tetiana Sobchyshyna <43253717+TetianaSob@users.noreply.github.com>
2019-11-24 14:26:49 -0500

    MyAlbums.php
    MyAlbums.php

--- MyAlbums.php
+++ MyAlbums.php
@@ -2,11 +2,9 @@
 
             <?php
             include ('./ProjectCommon/Functions.php');
-            include('./ProjectCommon/Header.php');
-            include('./ProjectCommon/Footer.php');
             session_start();
             $connection = ConnectDb();
-            
+            include('./ProjectCommon/Header.php');
             ?>
 
 
@@ -106,22 +104,14 @@
                     <div class="col-md-2"><button type="submit" id="btnSaveChanges" name="btnSaveChanges" class="btn btn-primary">Save Changes</button></div>
 
                 </div>
-                    
-
-        </div>
-        </form>
-
-
+</form>
+     
     <div class="push"></div>
+  </div>
 
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+<?php include('./ProjectCommon/Footer.php'); ?>
 
-    <!-- front part end -->
-
-
-</body>  
+</body>     
 </html>
 
 
