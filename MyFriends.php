4e30be9e5b37f6b82f9502daf61e607d10f537fd
elric25 <boud0137@algonquinlive.com>
2019-12-01 10:06:07 -0500

    Login validation for myfriends

--- MyFriends.php
+++ MyFriends.php
@@ -1,3 +1,26 @@
+<?php
+    session_start();
+    if($_SESSION['loggedIn'] == null)
+    {
+        header("location: Login.php");
+    }
+    else
+    {
+        $LoginActive = 'hide';
+        $LogoutActive = 'display';
+    }
+    include("./Common/functions.php");
+
+    $connection = ConnectDb();
+    $usernameQuery = "SELECT Name FROM User WHERE UserId = '$_SESSION[loggedIn]'";
+    $usernameResult = $connection->query($usernameQuery);
+    $usernames = $usernameResult->fetch_assoc();
+    $username = $usernames["Name"];
+    
+    $Error = "";
+    
+?>
+
 <?php include("./Common/header.php"); ?>
     <link rel="stylesheet" href="Contents/Site.css">
     <div class="horizontal-margin vertical-margin">
