bf6e7c37bfd02cd9d1a91d17225210e47d2c15aa
elric25 <boud0137@algonquinlive.com>
2019-12-01 18:13:31 -0500

    Updated mypictures
    Also added a folder for user-uploaded pictures

--- MyPictures.php
+++ MyPictures.php
@@ -1,104 +1,130 @@
 <?php
-    $PictureActive = 'active';
-    session_start();
-    if($_SESSION['loggedIn'] == null)
-    {
-        header("location: Login.php");
-    }
-    else
-    {
-        $LoginActive = 'hide';
-        $LogoutActive = 'display';
-    }
-    include("./Common/functions.php");
+$PictureActive = 'active';
+session_start();
+if ($_SESSION['loggedIn'] == null) {
+    header("location: Login.php");
+} else {
+    $LoginActive = 'hide';
+    $LogoutActive = 'display';
+}
+include("./Common/functions.php");
 
-    $connection = ConnectDb();
-    $usernameQuery = "SELECT Name FROM User WHERE UserId = '$_SESSION[loggedIn]'";
-    $usernameResult = $connection->query($usernameQuery);
-    $usernames = $usernameResult->fetch_assoc();
-    $username = $usernames["Name"];
-    
-    $Error = "";
-    
+$connection = ConnectDb();
+$usernameQuery = "SELECT Name FROM User WHERE UserId = '$_SESSION[loggedIn]'";
+$usernameResult = $connection->query($usernameQuery);
+$usernames = $usernameResult->fetch_assoc();
+$username = $usernames["Name"];
+
+$Error = "";
+
+if (!isset($_GET['album'])) {
+    $query = "SELECT * FROM Album WHERE Owner_Id='$_SESSION[loggedIn]'";
+    $result = $connection->query($query);
+    $row = $result->fetch_assoc();
+    $_GET['album'] = $row['Album_Id'];
+}
+
+$picture['Picture_Id'] = isset($_GET['picture']) ? $_GET['picture'] : "";
+$pictureList = [];
+$query = "SELECT *, DATE_FORMAT(Date_added,'%d/%m/%Y') AS Date_uploaded FROM Picture WHERE Album_Id = '$_GET[album]'";
+$result = $connection->query($query);
+
+while ($row = $result->fetch_assoc()) {
+    if ($row['Picture_Id'] == $picture['Picture_Id'] || $picture['Picture_Id'] == "") {
+        $picture = $row;
+    }
+    $pictureList[] = $row;
+}
+
+if (isset($_POST['btnComment'])) {
+    $query = "INSERT INTO Comment (Author_Id, Picture_Id, Comment_Text, Date) VALUES ('$_SESSION[loggedIn]', '$_POST[picture_id]', '$_POST[comment]', CURRENT_TIMESTAMP)";
+    $connection->query($query);
+}
 ?>
 
 <?php include("./Common/header.php"); ?>
-    <link rel="stylesheet" href="Contents/Site.css">
-    <div class="horizontal-margin vertical-margin">
-    <h2>My Pictures</h2>        
-        
-        
-        
-<form id="PictureForm" method="GET" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+<link rel="stylesheet" href="Contents/Site.css">
+<div class="horizontal-margin vertical-margin">
+    <h1 style="text-align: center">My Pictures</h1>        
     <table style="width: 80%;">
-        <tr><td></br></td></tr>
         <tr>
-        <td class="style1">
-        <select name='accessibility' class='accessibility'>
-        <?php
-        $query = "SELECT Album_Id, Title, Date_Updated FROM Album WHERE Owner_Id = '$_SESSION[loggedIn]'";
-        $result = $connection->query($query);
-    
-        while($row = $result->fetch_assoc())
-        {
-            echo '<option value="'.$row['Album_Id'].'">'.$row['Title'].'- updated on '.$row['Date_Updated'].'</option>';
-        }
-        ?>
-        </select>
-                          
+            <td>
+                <form action="" method="get">
+                    <select name='album' class='form-control' onchange="$(this).closest('form').trigger('submit')">
+                        <?php
+                        $query = "SELECT * FROM Album WHERE Owner_Id='$_SESSION[loggedIn]'";
+                        $result = $connection->query($query);
+
+                        while ($row = $result->fetch_assoc()) {
+                            if ($row['Album_Id'] == $_GET['album']) {
+                                echo '<option selected value="' . $row['Album_Id'] . '">' . $row['Title'] . ' - updated on ' . $row['Date_Updated'] . '</option>';
+                            } else {
+                                echo '<option value="' . $row['Album_Id'] . '">' . $row['Title'] . ' - updated on ' . $row['Date_Updated'] . '</option>';
+                            }
+                        }
+                        ?>
+                    </select>
+                </form>
+            </td>
         </tr>
         <tr>
-        <td class="style1" colspan="3">
-<!--        //
-            TODO: Title goes here 
-        //-->
-        </td>   
-        <td class="style2">
-          
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-        </td>
+            <td>
+                <?php
+                echo "<h2 style='text-align: center'>$picture[Title]</h2>";
+                ?>
+            </td>
         </tr>
         <tr>
-        <td class="style1" colspan="3">
-           
-        </td>
-        <td>
-        
-        <div class="form-group" >
-        <textarea readonly class="form-control" id="commentbox" rows="20" cols="5"  style="background-color: rgba(130, 181, 224, 0.8)">Content</textarea>
-        </div>    
-        
-        <div class="form-group">
-        <input class="form-control" type="text" placeholder="Your comments" />
-        </div>
-        <div class="form-group">
-        <button class="btn btn-primary">Add Comment</button>
-        </div>
-        </form>
-        </div>
-        
-        </td>
+            <td rowspan="2" style="width: 60%; padding-right: 10px">
+                <div>
+                    <img src="./pictures/<?php
+                    echo $picture['FileName'];
+                    ?>" style="max-height: 400px; max-width: 100%;" />
+                </div>
+                <hr>
+                <div style="overflow-x: auto; width: 100%; white-space: nowrap;">
+                    <?php
+                    foreach ($pictureList as $pic) {
+                        echo "<a href='?album=$_GET[album]&picture=$pic[Picture_Id]'><img src='./pictures/$pic[FileName]' style='height: 100px;' /></a>";
+                    }
+                    ?>
+                </div>
+            </td>
+            <td style="padding-top: 10px">
+                <div style="max-height: 300px; overflow-y: auto;">
+                    <b>Description:</b>
+                    <div>
+                        <?php echo $picture["Description"] ?>
+                    </div>
+                    <b>Comments:</b>
+                    <div>
+                        <?php
+                        $query = "SELECT Comment.*, User.Name FROM Comment JOIN User ON Author_Id=UserId WHERE Picture_Id='$picture[Picture_Id]'";
+                        $result = $connection->query($query);
+
+                        while ($row = $result->fetch_assoc()) {
+                            echo "<div style='padding-top: 10px;'><span style='color:blue; font-style: italic;'>$row[Name] ($row[Date]]):</span>&nbsp$row[Comment_Text]</div>";
+                        }
+                        ?>
+                    </div>
+                </div>
+            </td>
         </tr>
-        
-        
-    </table>   
-    </div>
+        <tr>
+            <td>
+                <form method="POST" action="">
+                    <input type="hidden" value="<?php echo $picture['Picture_Id']; ?>" name="picture_id" />
+                    <div>
+                        <textarea rows="5" cols="5" type="text" name="comment"></textarea>
+                    </div>
+                    <div>
+                        <input type="submit" name="btnComment" value="Comment" class="btn btn-primary" />
+                    </div> 
+                </form>
+            </td>
+        </tr>
+    </table>
+
+</div>
 
 <?php include('./Common/footer.php'); ?>
