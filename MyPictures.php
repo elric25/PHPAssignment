93964041484d92ca7e48a0329942b9cc7f46bae2
lau00041 <lau00041@algonquinlive.com>
2019-12-01 17:33:50 -0500

    mypictures testing

--- MyPictures.php
+++ MyPictures.php
@@ -61,7 +61,84 @@
             
             
             
-            
+                         <div class="row">
+                    <!-- label --> 
+                    <div class="col-md-1"></div>
+                    <div class="col-md-6" id="inputText">
+                        <img src="images/Canadian-Parliament-3.jpg" width="800" title="imageFromDatabase" alt="imageFromDatabase">
+                        <br>
+                                                <div class="scrollmenu" width="800">
+
+                            <a href="" class="imageScroll"><img src="images/Canadian-Parliament-3.jpg" alt="" width="200px" height="120px" id="imgScroll"></a>
+                            <a href="" class="imageScroll"><img src="images/gettyimages-514479606.jpg" alt="" width="200px" height="120px" id="imgScroll"></a>
+                            <a href="" class="imageScroll"><img src="images/chateau-frontenac.jpg" alt="" width="200px" height="120px" id="imgScroll"></a>
+                            <a href="" class="imageScroll"><img src="images/Chateau-Frontenac-Quebec.jpg" alt="" width="200px" height="120px" id="imgScroll"></a>
+                            <a href="" class="imageScroll"><img src="images/altar.jpg" alt="" width="200px" height="120px" id="imgScroll"></a>
+                            <a href="" class="imageScroll"><img src="images/Summer-2015-Skyline0_5e954389-5056-a36f-234589b46e9b25ae.jpg" alt="" width="200px" height="120px" id="imgScroll"></a>
+                            <a href="" class="imageScroll"><img src="images/torontoskyline_8-250763.jpg" alt="" width="200px" height="120px" id="imgScroll"></a>
+                            <a href="" class="imageScroll"><img src="images/wallpaper-tags-toronto-night-city-city-lights-share-this-wallpaper.jpg" alt="" width="200px" height="120px" id="imgScroll"></a>
+                            <a href="" class="imageScroll"><img src="images/Untitled-design-40.jpg" alt="" width="200px" height="120px" id="imgScroll"></a>
+                            <a href="" class="imageScroll"><img src="images/5b.jpg" alt="" width="200px" height="120px" id="imgScroll"></a>
+                            <a href="" class="imageScroll"><img src="images/1200px-AGO_at_dusk.jpg" width="200px" height="120px" alt="" id="imgScroll"></a>
+                            <a href="" class="imageScroll"><img src="images/ROM_Exterior_406EC7A3-BE4C-4C08-83BF1C20A4F195D2_c63e8e8b-1e48-457f-bc3fd5ee7f1b9563.jpg" alt="" width="200px" height="120px" id="imgScroll"></a>
+                            <a href="" class="imageScroll"><img src="images/shutterstock_673595569.jpg" alt="" width="200px" height="120px" id="imgScroll"></a>
+                            <a href="" class="imageScroll"><img src="images/royal-ontario-museum-wedding-1.jpg" alt="" width="200px" height="120px" id="imgScroll"></a>
+                        </div>
+
+    <style>
+        div.scrollmenu {
+  background-color: #333;
+  overflow: auto;
+  white-space: nowrap;
+  
+  height:135px;
+ width: 900px;
+ margin-bottom: 50px;
+}
+
+div.scrollmenu a {
+  display: inline-block;
+  color: white;
+  text-align: center;
+  padding: 1px;
+  text-decoration: none;
+}
+
+div.scrollmenu a:hover {
+  background-color: #777;
+}
+    </style>
+                        <style>
+
+.photobanner {
+ height: 233px;
+ width: 900px;
+ margin-bottom: 50px;
+}
+ 
+.imageScroll{
+ -webkit-transition: all 0.5s ease;
+ -moz-transition: all 0.5s ease;
+ -o-transition: all 0.5s ease;
+ -ms-transition: all 0.5s ease;
+ transition: all 0.5s ease;
+}
+ 
+.imageScroll:hover {
+ -webkit-transform: scale(1.1);
+ -moz-transform: scale(1.1);
+ -o-transform: scale(1.1);
+ -ms-transform: scale(1.1);
+ transform: scale(1.1);
+ cursor: pointer;
+ 
+ -webkit-box-shadow: 0px 3px 5px rgba(0,0,0,0.2);
+ -moz-box-shadow: 0px 3px 5px rgba(0,0,0,0.2);
+ box-shadow: 0px 3px 5px rgba(0,0,0,0.2);
+}
+                        </style>
+
+   
             
             
             
@@ -77,45 +154,14 @@
         </tr>
         <tr>
         <td class="style1" colspan="3">
-         
-            
-            
-            
-        
-        </div>
-        <select multiple class="form-control" id="exampleFormControlSelect2" style="background-color: rgba(130, 181, 224, 0.8)">
-            <option name="myPicturesDescription">Description</option>
-            <option name="myPicturesComments">Comments</option>
-
-        </select>
-    
-            
-            
-            
-            
+           
         </td>
         <td>
         
-        <div class="form-group">
-        <textarea readonly class="form-control rounded-0" id="exampleFormControlTextarea1" rows="10"></textarea>
+        <div class="form-group" >
+        <textarea readonly class="form-control" id="commentbox" rows="20" cols="5"  style="background-color: rgba(130, 181, 224, 0.8)">Content</textarea>
         </div>    
-            
-            
-        <!--Div for css if we get around to it-->
         
-        <p class="commentDescription">Picture Description goes here.</p>
-        <div class="actionBox" >
-        <ul class="commentList">
-        <li>
-        <div class="Username">
-        Username (Date):
-        </div>
-        <div class="commentText">
-        <p class="">Hello this is a test comment.</p> 
-        </div>
-        </li>
-        </ul>
-        <form class="form-inline" role="form">
         <div class="form-group">
         <input class="form-control" type="text" placeholder="Your comments" />
         </div>
