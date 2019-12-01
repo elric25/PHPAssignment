946e2b5f3a43a8445c990f850c5b7ef6a4d70aa7
lau00041 <lau00041@algonquinlive.com>
2019-11-30 20:45:03 -0500

    update on AddAlbum

--- AddAlbum.php
+++ AddAlbum.php
@@ -2,30 +2,66 @@
     session_start();
     if($_SESSION['loggedIn'] == null)
     {
-        $LogoutActive = 'hide';
-        $LoginActive = 'display';
+        header("location: Index.php");
     }
     else
     {
         $LoginActive = 'hide';
         $LogoutActive = 'display';
     }
+    include("./Common/functions.php");
+
+    $connection = ConnectDb();
+    $usernameQuery = "SELECT Name FROM User WHERE UserId = '$_SESSION[loggedIn]'";
+    $usernameResult = $connection->query($usernameQuery);
+    $usernames = $usernameResult->fetch_assoc();
+    $username = $usernames["Name"];
     
+    $Error = "";
+    
+    session_start();
+    if(isset($_GET["reset"]))
+    {
+        $_GET["title"] = "";
+        $_GET["accessibility"] = "";
+        $_GET["description"] = "";
+    }
+
+    if(isset($_GET["btnSubmit"]))
+    {
+        $title = $_GET["title"];
+        $accessibilty = $_GET["accessibility"];
+        $description = $_GET["description"];
+        
+        $titleQuery = "SELECT Title FROM Album WHERE (Owner_Id = '$_SESSION[loggedIn]' && Title = '$title')";
+        $titleResult = $connection->query($titleQuery);
+        $titles = $titleResult->fetch_assoc();
+        
+        if(strlen($title) == null)
+        {
+            $Error = "*A title is required.";
+        }
+        //check if an album of the same name already exists for this user
+        else if($titles->num_rows > 0)
+        {
+            $Error = "*Album with this title already exists.";
+        }
+        else
+        {
+            //add album
+        }
+    }
+             
+     
 ?>
 
 <?php include("./Common/header.php"); ?>
     <link rel="stylesheet" href="Contents/Site.css">
     <div class="horizontal-margin vertical-margin">
-	<h2> Welcome to Algonquin Social Media</h2>        
-        <ul>
-            <li>
-                <p>If you have never used this before, you have to <a href="NewUser.php">sign up</a> first.</p>
-            </li>
-            <li>
-                <p>If you have already signed up, you can <a href="Login.php">log in</a> now</p>
-            </li>
-            <li><p><a href="phpinfo.php">Check browser specs</p></li>
-        </ul>
+	<h2>Create New Album</h2>        
+        Welcome <b><?php echo $username; ?>!</b> (Not you? Change user <a href='Login.php'>here</a>)
+        You have registered <br><br>
     </div>
-
+    
+    
 <?php include('./Common/footer.php'); ?>
