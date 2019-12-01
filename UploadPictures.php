e9adc0f6d99f2e64de97750650789c35b299a3ca
lau00041 <lau00041@algonquinlive.com>
2019-12-01 04:58:09 -0500

    Update UploadPictures.php

--- UploadPictures.php
+++ UploadPictures.php
@@ -29,15 +29,6 @@
     //
     //          DROPDOWN
     //
-    $query = "SELECT Album_Id, Title FROM Album WHERE Owner_Id = '$_SESSION[loggedIn]'";
-    $result = $connection->query($query);
-    
-    print "<select name='accessibility' class='accessibility'>";
-    while($row = $result->fetch_assoc())
-    {
-        echo '<option value="'.$row['Album_Id'].'">'.$row['Title'].'</option>';
-    }
-    print "</select>";
     
     
     
@@ -52,6 +43,79 @@
             <p>You can upload multiple pictures at a time by pressing the shift key while selecting pictures.</p>
             <p>When uploading multiple pictures, the title and description fields will be applied to all pictures.</p>
         </ul>
-    </div>
+        
+    <form id="UploadForm" method="GET" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+        <table style="width: 80%;">
+        <tr><td></br></td></tr>
+        <tr>
+        <td class="style1" colspan="3">
+        Upload to Album: 
+        </td>
+        <td class="style2">
+        <label>
+        <select name='accessibility' class='accessibility'>
+        <?php
+        $query = "SELECT Album_Id, Title FROM Album WHERE Owner_Id = '$_SESSION[loggedIn]'";
+        $result = $connection->query($query);
+    
+        while($row = $result->fetch_assoc())
+        {
+            echo '<option value="'.$row['Album_Id'].'">'.$row['Title'].'</option>';
+        }
+        ?>
+        </select>
+        </label>
+        </td>                       
+        </tr>
+        <tr>
+        <td class="style1" colspan="3">
+        File to Upload: 
+        </td>   
+        <td class="style2">
+<!--        //
+        // TODO: form {
+                  display: inline-block;
+                  position: relative;
+                }
+                form input {
+                  padding-right: 4.6em;
+                }
+                form button {
+                  position: absolute;
+                  top: 0;
+                  right: 0;
+                  width: 4.4em;
+                }
+        //-->
+        <input class="form-control" type="text" name="choose" id="choose" value="<?php echo $_GET["choose"]; ?>">
+        <button type="button" id="btnChoosePictures" name="btnChoosePictures" class="btn btn-secondary">Choose Files</button>
+        </td>
+        </tr>
+        <tr>
+        <td class="style1" colspan="3">
+        Title: 
+        </td>
+        <td>
+            <input class="form-control" type="text" name="choose" id="choose" value="<?php echo $_GET["choose"]; ?>">
+        </td>
+        </tr>
+        <tr>
+        <td class="style1" colspan="3">
+        Description:        
+        </td>
+        <td>
+            <textarea rows="7" cols="20" type="text" id="description" name='description'><?php echo $_GET["description"]; ?></textarea>    
+        </td>
+        </tr>
+        
+        
+        </table>
+        <input type="submit" name="btnSubmit" id="btnSubmit" value="Submit" class="btn btn-primary">
+        <input type="submit" name="reset" id="reset" value="Clear" class="btn btn-primary">
+        </form>
 
+    </div>
+    
+    
+    
 <?php include('./Common/footer.php'); ?>
