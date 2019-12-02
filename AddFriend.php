d2a68496b517c39a1c7ff195721aa16a2327ec2a
lau00041 <lau00041@algonquinlive.com>
2019-12-02 14:58:50 -0500

    Update AddFriend.php

--- AddFriend.php
+++ AddFriend.php
@@ -12,12 +12,35 @@
     include("./Common/functions.php");
 
     $connection = ConnectDb();
-    $usernameQuery = "SELECT Name FROM User WHERE UserId = '$_SESSION[loggedIn]'";
+    $userID = $_SESSION["loggedIn"];
+    $usernameQuery = "SELECT Name FROM User WHERE UserId = '$userID'";
     $usernameResult = $connection->query($usernameQuery);
     $usernames = $usernameResult->fetch_assoc();
     $username = $usernames["Name"];
     
+    
     $Error = "";
+    
+    $friendID = $_GET["friendID"];
+    
+    $friendQuery = "SELECT UserId FROM User WHERE UserId = '$friendID'";
+    $friendResult = $connection->query($friendQuery);
+    if($friendResult->num_rows > 0)
+    {
+        $friends = $friendResult->fetch_assoc();
+        $friendIdDB = $friends["User_Id"];
+    }
+    else
+    {
+        $Error = "A user with this ID does not exist";
+    }
+    $statusQuery = "SELECT Status_Code FROM FriendshipStatus WHERE Status_Code = 'request'";
+    $statusResult = $connection->query($statusQuery);
+    $status = $statusResult->fetch_assoc();
+    $request = $status["Status_Code"];
+    
+    $insert = "INSERT INTO Project.Friendship (FriendRequesteeId, Friend_RequesterId, Status) "
+        . "VALUES ('$userID', '$friendIdDB', '$phone', '$passwordHashed')";
     
 ?>
 
@@ -33,14 +56,13 @@
         <form id="addFriendForm" method="GET" action="<?php echo $_SERVER['PHP_SELF']; ?>">
         <table style="width: 80%;">
         <tr><td></br></td></tr>
+        <?php echo '<div style="Color:red">'.$Error.'</div>'; ?> 
         <tr>
         <td class="style1" colspan="3">
         ID:
         </td>
         <td class="style2" colspan="3">
         <input type="text" id="friendId" name='friendId'>
-        </td>
-        <td class="style3" colspan="3">
         <input type="submit" name="btnSubmit" id="btnSubmit" value="Send Friend Request" class="btn btn-primary">
         </td>
         </tr>
