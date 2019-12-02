74c9f0eda0ee964148098f6ff22cfaddd7f4e295
lau00041 <lau00041@algonquinlive.com>
2019-12-02 16:06:25 -0500

    AddFriend Done

--- AddFriend.php
+++ AddFriend.php
@@ -11,37 +11,67 @@
     }
     include("./Common/functions.php");
 
+    //connection to DB
     $connection = ConnectDb();
+    
+    //Getting userinfo
     $userID = $_SESSION["loggedIn"];
     $usernameQuery = "SELECT Name FROM User WHERE UserId = '$userID'";
     $usernameResult = $connection->query($usernameQuery);
     $usernames = $usernameResult->fetch_assoc();
     $username = $usernames["Name"];
     
-    
+    //predeclared Error
     $Error = "";
     
-    $friendID = $_GET["friendID"];
-    
-    $friendQuery = "SELECT UserId FROM User WHERE UserId = '$friendID'";
-    $friendResult = $connection->query($friendQuery);
-    if($friendResult->num_rows > 0)
+    if(isset($_GET["btnSubmit"]))
     {
-        $friends = $friendResult->fetch_assoc();
-        $friendIdDB = $friends["User_Id"];
+        //Friend ID check
+        $friendID = $_GET["friendID"];
+        
+        if($friendID == null)
+        {
+            $Error = "You must enter a Friend ID";
+        }
+        else
+        {
+            $friendQuery = "SELECT UserId FROM User WHERE UserId = '$friendID'";
+            $friendResult = $connection->query($friendQuery);
+            $friendIdDB;
+            if($friendResult->num_rows > 0)
+            {
+                $friends = $friendResult->fetch_assoc();
+                $friendIdDB = $friends["UserId"];
+            }
+            else
+            {
+                $Error = "A user with this ID does not exist";
+            }
+
+            //Getting request variable instead of hardcoding
+            $statusQuery = "SELECT Status_Code FROM FriendshipStatus WHERE Status_Code = 'request'";
+            $statusResult = $connection->query($statusQuery);
+            $status = $statusResult->fetch_assoc();
+            $request = $status["Status_Code"];
+
+            //Check if they are already friends or have a request send before adding
+            $check = "SELECT * FROM Friendship WHERE Friend_RequesteeId = '$userID' AND Friend_RequesterId = '$friendIdDB'";
+            $checkResult = $connection->query($check);
+            if($checkResult->num_rows > 0)
+            {
+                $Error = "You already sent a friend request or are already friends with this user.";
+            }
+            else
+            {       
+                $insert = "INSERT INTO Project.Friendship (Friend_RequesteeId, Friend_RequesterId, Status) "
+                . "VALUES ('$userID', '$friendIdDB', '$request')";
+                $connection->query($insert);
+                header("location: MyFriends.php");
+            }
+        }
+        
+        
     }
-    else
-    {
-        $Error = "A user with this ID does not exist";
-    }
-    $statusQuery = "SELECT Status_Code FROM FriendshipStatus WHERE Status_Code = 'request'";
-    $statusResult = $connection->query($statusQuery);
-    $status = $statusResult->fetch_assoc();
-    $request = $status["Status_Code"];
-    
-    $insert = "INSERT INTO Project.Friendship (FriendRequesteeId, Friend_RequesterId, Status) "
-        . "VALUES ('$userID', '$friendIdDB', '$phone', '$passwordHashed')";
-    
 ?>
 
 <?php include("./Common/header.php"); ?>
@@ -52,7 +82,6 @@
             Welcome <b><?php echo $username; ?>!</b> (Not you? Change user <a href='Login.php'>here</a>)
             <p>Enter the ID of the user you want to be friend with</p>
         </ul>
-        
         <form id="addFriendForm" method="GET" action="<?php echo $_SERVER['PHP_SELF']; ?>">
         <table style="width: 80%;">
         <tr><td></br></td></tr>
@@ -62,7 +91,7 @@
         ID:
         </td>
         <td class="style2" colspan="3">
-        <input type="text" id="friendId" name='friendId'>
+        <input type="text" id="friendId" name='friendID'>
         <input type="submit" name="btnSubmit" id="btnSubmit" value="Send Friend Request" class="btn btn-primary">
         </td>
         </tr>
