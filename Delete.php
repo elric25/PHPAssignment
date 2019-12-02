7dacc46c8d39aaf156c88fa3a9ad3403bcb27097
elric25 <boud0137@algonquinlive.com>
2019-12-02 09:14:05 -0500

    removed un-needed lines

--- Delete.php
+++ Delete.php
@@ -4,11 +4,11 @@ session_start();
 include("./Common/functions.php");
 
 $connection = ConnectDb();
-$usernameQuery = "SELECT Name FROM User WHERE UserId = '$_SESSION[loggedIn]'";
-$usernameResult = $connection->query($usernameQuery);
-$usernames = $usernameResult->fetch_assoc();
-$username = $usernames["Name"];
-    
+//$usernameQuery = "SELECT Name FROM User WHERE UserId = '$_SESSION[loggedIn]'";
+//$usernameResult = $connection->query($usernameQuery);
+//$usernames = $usernameResult->fetch_assoc();
+//$username = $usernames["Name"];
+//    
 $Error = "";
 $AlbumID = $_GET['album'];
 $query1 = "DELETE * FROM Picture WHERE Album_Id = '$AlbumID'";
